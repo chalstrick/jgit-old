3db75a1cdd0901927e5536b229c537cfc3519148
Christian Halstrick <christian.halstrick@sap.com>
2010-09-29 09:37:06 +0200

    enhance probe again and again
    Change-Id: Ib470e43f9a8bcb9d69d59723bf2207b382452d26

--- org.eclipse.jgit.test/tst/myprobe_probe.java
+++ org.eclipse.jgit.test/tst/myprobe_probe.java
@@ -7,7 +7,7 @@ class myprobe_probe {
   public static class Probe_0 {
     // Fragment at class scope
 public static String spaces = "";
-public static boolean previousEnter = false;
+public static boolean lastOpWasEntry = false;
     public static void _entry (
       String /*className*/ aclassName0,
       String /*methodName*/ amethodName1,
@@ -16,9 +16,12 @@ public static boolean previousEnter = false;
       Object[] /*args*/ aargs4      ) {
       // Internal signature for this method: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;[Ljava/lang/Object;)V
 //------------------ begin user-written fragment code ----------------
+if (lastOpWasEntry)
+	System.out.print("{");
 System.out.print("\n"+spaces+aclassName0+"."+amethodName1+"("+java.util.Arrays.asList(aargs4)+") ");
 spaces = spaces + "  ";
-previousEnter = true;
+lastOpWasEntry = true;
+
 
 
 //------------------- end user-written fragment code -----------------
@@ -29,15 +32,11 @@ previousEnter = true;
       String /*methodName*/ amethodName1      ) {
       // Internal signature for this method: (Ljava/lang/Object;Ljava/lang/String;Ljava/lang/String;)V
 //------------------ begin user-written fragment code ----------------
-if (!previousEnter) {
-	System.out.print("\n"+spaces+"return "+areturnedObject);
-	spaces = spaces.substring(2);
-	System.out.print("\n"+spaces+"}");
-} else {
-	System.out.print(": "+areturnedObject);
-	spaces = spaces.substring(2);
-}
-previousEnter = false;
+spaces = spaces.substring(2);
+if (!lastOpWasEntry)
+	System.out.print("\n" + spaces + "} ");
+System.out.print("=> " + areturnedObject);
+lastOpWasEntry = false;
 
 //------------------- end user-written fragment code -----------------
     }
