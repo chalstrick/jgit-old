eclipse-jgit/jgit

Fix promptPassphrase() to ask for CredentialItem.Password
promptPassphrase() was using the wrong CredentialItem type.

Change-Id: Ia1f9ecbaf3b67fe6213324ad9231925780da7e24
Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.jgit/src/org/eclipse/jgit/transport/CredentialsProviderUserInfo.java
@@ -94,9 +94,9 @@ public class CredentialsProviderUserInfo implements UserInfo,
 	}
 
 	public boolean promptPassphrase(String msg) {
-		CredentialItem.StringType v = newPrompt(msg);
-		if (provider.get(uri, v)) {
-			passphrase = v.getValue();
+		CredentialItem.Password p = new CredentialItem.Password(msg);
+		if (provider.get(uri, p)) {
+			passphrase = new String(p.getValue());
 			return true;
 		} else {
 			passphrase = null;
@@ -113,10 +113,6 @@ public class CredentialsProviderUserInfo implements UserInfo,
 			password = null;
 			return false;
 		}
-	}
-
-	private CredentialItem.StringType newPrompt(String msg) {
-		return new CredentialItem.StringType(msg, true);
 	}
 
 	public boolean promptYesNo(String msg) {

chalstrick/jgit-old

org.eclipse.jgit/src/org/eclipse/jgit/transport/CredentialsProviderUserInfo.java
@@ -94,9 +94,9 @@ public class CredentialsProviderUserInfo implements UserInfo,
 	}
 
 	public boolean promptPassphrase(String msg) {
-		CredentialItem.StringType v = newPrompt(msg);
-		if (provider.get(uri, v)) {
-			passphrase = v.getValue();
+		CredentialItem.Passphrase p = new CredentialItem.Passphrase(msg);
+		if (provider.get(uri, p)) {
+			passphrase = new String(p.getValue());
 			return true;
 		} else {
 			passphrase = null;
@@ -113,10 +113,6 @@ public class CredentialsProviderUserInfo implements UserInfo,
 			password = null;
 			return false;
 		}
-	}
-
-	private CredentialItem.StringType newPrompt(String msg) {
-		return new CredentialItem.StringType(msg, true);
 	}
 
 	public boolean promptYesNo(String msg) {